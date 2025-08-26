4194f47937560f29e495d694d66171866e572907
Motunrayo Sanni <sannimotunrayo04@gmail.com>
2025-08-26 11:40:03 +0100

    added

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Sidebar from '../components/Sidebar';
-import Navbar from '../components/navbar';
+import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom';
 
 export default function Layout() {
